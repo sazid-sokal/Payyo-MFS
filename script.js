e14d7cd0c4f16ebf5944aff5cf32acf4b8d0b9aa
sokal <ls>
2024-09-21 12:56:57 +0600

    log in form done

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.getElementById('login-btn').addEventListener('click', function(event){
     event.preventDefault();
     console.log("shala click koireche");
-    const phoneNumebr = document.getElementById('phone-number');
-    console.log('phone number: ' + phoneNumebr.value);
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log('phone number: ' + phoneNumber + ' ; ' + 'pin: ' + pinNumber);
+
+    // validation
+    if (phoneNumber == 'admin' && pinNumber == 'admin'){
+        console.log('you are logged in');
+        window.location.href = './home.html';
+    }
+    else{
+        alert('phone number or pin code is wrong')
+    }
 })
